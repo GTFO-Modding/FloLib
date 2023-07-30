1b146fff8c73442b52598efb19a9cba101511a24
Flowaria <wnsghgj1234@naver.com>
2023-07-30 12:31:26 +0900

    Implemented ScreenShake

---
+++ FloLib/Game/ScreenShakes/ScreenShake.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx.Unity.IL2CPP.Utils;
+using FloLib.Infos;
+using FloLib.Networks;
+using FloLib.Utils;
+using Player;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FloLib.Game.ScreenShakes;
+public sealed class ScreenShake
+{
+    [AutoInvoke(InvokeWhen.StartupAssetLoaded)]
+    internal static void Init()
+    {
+        GlobalNetAction<ScreenShakeDescriptor>.OnReceive += OnReceive;
+    }
+
+    public static void Trigger(ScreenShakeDescriptor data)
+    {
+        GlobalNetAction<ScreenShakeDescriptor>.Send(data);
+    }
+
+    private static void OnReceive(ulong sender, ScreenShakeDescriptor data)
+    {
+        if (!LocalPlayer.TryGetAgent(out var localPlayer))
+            return;
+
+        var camera = localPlayer.FPSCamera;
+        camera.StartCoroutine(DoShake(camera, data));
+    }
+
+    private static IEnumerator DoShake(FPSCamera camera, ScreenShakeDescriptor data)
+    {
+        var time = 0.0f;
+        while (time < data.Duration)
+        {
+            var distFactor = 1.0f;
+            if (data.Mode == ScreenShakeMode.PositionFalloff)
+            {
+                var distance = Vector3.Distance(data.Position, camera.Position);
+                distFactor = Mathf.InverseLerp(data.FalloffEnd, data.FalloffStart, distance);
+            }
+
+            var timeFactor = data.Modifier switch
+            {
+                ScreenShakeModifier.PingPong => Mathf.PingPong(Mathf.InverseLerp(data.Duration, 0.0f, time) * 2.0f, 1.0f),
+                ScreenShakeModifier.Increase => Mathf.InverseLerp(0.0f, data.Duration, time),
+                ScreenShakeModifier.Decrease => Mathf.InverseLerp(data.Duration, 0.0f, time),
+                _ => 1.0f
+            };
+
+            timeFactor = data.IntensityEasing.Evaluate(timeFactor);
+
+            var amount = distFactor * timeFactor * data.Intensity;
+            camera.SetConstantCameraShakeAmount(amount);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        camera.SetConstantCameraShakeAmount(0.0f);
+    }
+}
---
+++ FloLib/Game/ScreenShakes/ScreenShakeDescriptor.cs
@@ -0,0 +1,37 @@
+﻿using FloLib.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FloLib.Game.ScreenShakes;
+/// <summary>
+/// 
+/// </summary>
+public struct ScreenShakeDescriptor
+{
+    public ScreenShakeMode Mode;
+    public ScreenShakeModifier Modifier;
+    public Vector3 Position;
+    public float Intensity;
+    public float Duration;
+    public float FalloffStart;
+    public float FalloffEnd;
+    public EaseFunc.Type IntensityEasing;
+}
+
+public enum ScreenShakeMode : byte
+{
+    Global,
+    PositionFalloff
+}
+
+public enum ScreenShakeModifier : byte
+{
+    PingPong,
+    Increase,
+    Decrease,
+    Constant,
+}
