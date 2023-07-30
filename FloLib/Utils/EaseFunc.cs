709c6e114976de61483eddda5a274ac14bec94ad
Flowaria <wnsghgj1234@naver.com>
2023-07-30 12:32:45 +0900

    Added Utils.EaseFunc

---
+++ FloLib/Utils/EaseFunc.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FloLib.Utils;
+public static class EaseFunc
+{
+    public enum Type : byte
+    {
+        Linear,
+		Zero,
+		One,
+
+        InQuad,
+        OutQuad,
+        InOutQuad,
+
+        InCubic,
+        OutCubic,
+        InOutCubic,
+
+        InQuart,
+        OutQuart,
+        InOutQuart,
+
+        InQuint,
+        OutQuint,
+        InOutQuint,
+
+        InSine,
+        OutSine,
+        InOutSine,
+
+        InExpo,
+        OutExpo,
+        InOutExpo,
+
+        InCirc,
+        OutCirc,
+        InOutCirc,
+
+        InElastic,
+        OutElastic,
+        InOutElastic,
+
+        InBack,
+        OutBack,
+        InOutBack,
+
+        InBounce,
+        OutBounce,
+        InOutBounce
+    }
+
+	public static float Evaluate(this Type type, float t)
+    {
+		t = Mathf.Clamp01(t);
+
+		return type switch
+		{
+			Type.Linear => t,
+			Type.Zero => 0.0f,
+			Type.One => 1.0f,
+			Type.InQuad => InQuad(t),
+			Type.OutQuad => OutQuad(t),
+			Type.InOutQuad => InOutQuad(t),
+			Type.InCubic => InCubic(t),
+			Type.OutCubic => OutCubic(t),
+			Type.InOutCubic => InOutCubic(t),
+			Type.InQuart => InQuart(t),
+			Type.OutQuart => OutQuart(t),
+			Type.InOutQuart => InOutQuart(t),
+			Type.InQuint => InQuint(t),
+			Type.OutQuint => OutQuint(t),
+			Type.InOutQuint => InOutQuint(t),
+			Type.InSine => InSine(t),
+			Type.OutSine => OutSine(t),
+			Type.InOutSine => InOutSine(t),
+			Type.InExpo => InExpo(t),
+			Type.OutExpo => OutExpo(t),
+			Type.InOutExpo => InOutExpo(t),
+			Type.InCirc => InCirc(t),
+			Type.OutCirc => OutCirc(t),
+			Type.InOutCirc => InOutCirc(t),
+			Type.InElastic => InElastic(t),
+			Type.OutElastic => OutElastic(t),
+			Type.InOutElastic => InOutElastic(t),
+			Type.InBack => InBack(t),
+			Type.OutBack => OutBack(t),
+			Type.InOutBack => InOutBack(t),
+			Type.InBounce => InBounce(t),
+			Type.OutBounce => OutBounce(t),
+			Type.InOutBounce => InOutBounce(t),
+			_ => throw new ArgumentOutOfRangeException(nameof(type), "Given type was invalid!")
+		};
+    }
+
+
+	public static float Linear(float t) => t;
+	/// <summary>
+	/// It just return 0.0
+	/// </summary>
+	/// <param name="t">This doesn't matter actually</param>
+	/// <returns>0.0</returns>
+	public static float Zero(float t) => 0.0f;
+	/// <summary>
+	/// It just return 1.0
+	/// </summary>
+	/// <param name="t">This doesn't matter actually</param>
+	/// <returns>1.0</returns>
+	public static float One(float t) => 1.0f;
+
+	public static float InQuad(float t) => t * t;
+	public static float OutQuad(float t) => 1 - InQuad(1 - t);
+	public static float InOutQuad(float t)
+	{
+		if (t < 0.5) return InQuad(t * 2) / 2;
+		return 1 - InQuad((1 - t) * 2) / 2;
+	}
+
+	public static float InCubic(float t) => t * t * t;
+	public static float OutCubic(float t) => 1 - InCubic(1 - t);
+	public static float InOutCubic(float t)
+	{
+		if (t < 0.5) return InCubic(t * 2) / 2;
+		return 1 - InCubic((1 - t) * 2) / 2;
+	}
+
+	public static float InQuart(float t) => t * t * t * t;
+	public static float OutQuart(float t) => 1 - InQuart(1 - t);
+	public static float InOutQuart(float t)
+	{
+		if (t < 0.5) return InQuart(t * 2) / 2;
+		return 1 - InQuart((1 - t) * 2) / 2;
+	}
+
+	public static float InQuint(float t) => t * t * t * t * t;
+	public static float OutQuint(float t) => 1 - InQuint(1 - t);
+	public static float InOutQuint(float t)
+	{
+		if (t < 0.5) return InQuint(t * 2) / 2;
+		return 1 - InQuint((1 - t) * 2) / 2;
+	}
+
+	public static float InSine(float t) => (float)-Math.Cos(t * Math.PI / 2);
+	public static float OutSine(float t) => (float)Math.Sin(t * Math.PI / 2);
+	public static float InOutSine(float t) => (float)(Math.Cos(t * Math.PI) - 1) / -2;
+
+	public static float InExpo(float t) => (float)Math.Pow(2, 10 * (t - 1));
+	public static float OutExpo(float t) => 1 - InExpo(1 - t);
+	public static float InOutExpo(float t)
+	{
+		if (t < 0.5) return InExpo(t * 2) / 2;
+		return 1 - InExpo((1 - t) * 2) / 2;
+	}
+
+	public static float InCirc(float t) => -((float)Math.Sqrt(1 - t * t) - 1);
+	public static float OutCirc(float t) => 1 - InCirc(1 - t);
+	public static float InOutCirc(float t)
+	{
+		if (t < 0.5) return InCirc(t * 2) / 2;
+		return 1 - InCirc((1 - t) * 2) / 2;
+	}
+
+	public static float InElastic(float t) => 1 - OutElastic(1 - t);
+	public static float OutElastic(float t)
+	{
+		float p = 0.3f;
+		return (float)Math.Pow(2, -10 * t) * (float)Math.Sin((t - p / 4) * (2 * Math.PI) / p) + 1;
+	}
+	public static float InOutElastic(float t)
+	{
+		if (t < 0.5) return InElastic(t * 2) / 2;
+		return 1 - InElastic((1 - t) * 2) / 2;
+	}
+
+	public static float InBack(float t)
+	{
+		float s = 1.70158f;
+		return t * t * ((s + 1) * t - s);
+	}
+	public static float OutBack(float t) => 1 - InBack(1 - t);
+	public static float InOutBack(float t)
+	{
+		if (t < 0.5) return InBack(t * 2) / 2;
+		return 1 - InBack((1 - t) * 2) / 2;
+	}
+
+	public static float InBounce(float t) => 1 - OutBounce(1 - t);
+	public static float OutBounce(float t)
+	{
+		float div = 2.75f;
+		float mult = 7.5625f;
+
+		if (t < 1 / div)
+		{
+			return mult * t * t;
+		}
+		else if (t < 2 / div)
+		{
+			t -= 1.5f / div;
+			return mult * t * t + 0.75f;
+		}
+		else if (t < 2.5 / div)
+		{
+			t -= 2.25f / div;
+			return mult * t * t + 0.9375f;
+		}
+		else
+		{
+			t -= 2.625f / div;
+			return mult * t * t + 0.984375f;
+		}
+	}
+	public static float InOutBounce(float t)
+	{
+		if (t < 0.5) return InBounce(t * 2) / 2;
+		return 1 - InBounce((1 - t) * 2) / 2;
+	}
+}
