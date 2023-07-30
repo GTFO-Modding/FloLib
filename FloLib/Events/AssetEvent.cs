d0a3dbadafb606cddf079ba7290ffa25c04b8f5e
Flowaria <wnsghgj1234@naver.com>
2023-07-30 12:27:24 +0900

    Added AssetEvent

---
+++ FloLib/Events/AssetEvent.cs
@@ -0,0 +1,74 @@
+﻿using AssetShards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloLib.Events;
+/// <summary>
+/// Events Related to <see cref="AssetShardManager"/>
+/// </summary>
+public static class AssetEvent
+{
+    /// <summary>
+    /// Invoked When <see cref="AssetShardManager.OnStartupAssetsLoaded"/> has invoked
+    /// </summary>
+    public static event Action OnStartupAssetLoaded;
+    /// <summary>
+    /// Invoked When <see cref="AssetShardManager.OnEnemyAssetsLoaded"/> has invoked
+    /// </summary>
+    public static event Action OnEnemyAssetLoaded;
+    /// <summary>
+    /// Invoked When <see cref="AssetShardManager.OnSharedAsssetLoaded"/> has invoked
+    /// </summary>
+    public static event Action OnSharedAssetLoaded;
+    /// <summary>
+    /// Invoked When all [<see cref="AssetShardManager.OnStartupAssetsLoaded"/>], [<see cref="AssetShardManager.OnEnemyAssetsLoaded"/>] and [<see cref="AssetShardManager.OnSharedAsssetLoaded"/>] has invoked
+    /// </summary>
+    public static event Action OnAllAssetsLoaded;
+
+    private static bool _IsStartupLoaded = false;
+    private static bool _IsEnemyLoaded = false;
+    private static bool _IsSharedLoaded = false;
+    private static bool _IsAllLoaded = false;
+
+    internal static void Invoke_StartupAssetLoaded()
+    {
+        if (_IsStartupLoaded)
+            return;
+
+        _IsStartupLoaded = true;
+        OnStartupAssetLoaded?.Invoke();
+        CheckAllAssetsLoaded();
+    }
+
+    internal static void Invoke_EnemyAssetLoaded()
+    {
+        if (_IsEnemyLoaded)
+            return;
+
+        _IsEnemyLoaded = true;
+        OnEnemyAssetLoaded?.Invoke();
+        CheckAllAssetsLoaded();
+    }
+
+    internal static void Invoke_SharedAssetLoaded()
+    {
+        if (_IsSharedLoaded)
+            return;
+
+        _IsSharedLoaded = true;
+        OnSharedAssetLoaded?.Invoke();
+        CheckAllAssetsLoaded();
+    }
+
+    private static void CheckAllAssetsLoaded()
+    {
+        if (_IsAllLoaded)
+            return;
+
+        _IsAllLoaded = true;
+        OnAllAssetsLoaded?.Invoke();
+    }
+}
---
+++ FloLib/Events/Inject/Inject_AssetShardManager.cs
@@ -0,0 +1,31 @@
+﻿using AssetShards;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloLib.Events.Inject;
+[HarmonyPatch(typeof(AssetShardManager), nameof(AssetShardManager.LoadAllShardsForBundleAsync))]
+internal static class Inject_AssetShardManager
+{
+    //necessary evil: This ensures the timing of assetLoaded callback always after base-game callback has finished.
+    internal static void Prefix(AssetBundleName name, ref Il2CppSystem.Action callback)
+    {
+        switch (name)
+        {
+            case AssetBundleName.Startup:
+                callback += (Action)AssetEvent.Invoke_StartupAssetLoaded;
+                break;
+
+            case AssetBundleName.Enemies:
+                callback += (Action)AssetEvent.Invoke_EnemyAssetLoaded;
+                break;
+
+            case AssetBundleName.Complex_Shared:
+                callback += (Action)AssetEvent.Invoke_SharedAssetLoaded;
+                break;
+        }
+    }
+}
